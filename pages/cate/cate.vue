d57006bd8b4d9f4906cec95dc1453d1b6dd9f754
zjm <2215539769@qq.com>
2023-09-07 20:19:54 +0800

    完成渲染二级三级列表

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -10,40 +10,21 @@
       </scroll-view>
 
       <!-- 右侧的滚动视图区域 -->
-      <scroll-view class="right-scroll-view" scroll-y :style="{height: wh + 'px'}">
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">zzz</view>
-        <view class="left-scroll-view-item">多复制一些节点，演示纵向滚动效果</view>
-      </scroll-view>
+<scroll-view class="right-scroll-view" scroll-y :style="{height: wh + 'px'}" :scroll-top="scrollTop">
+  <view class="cate-lv2" v-for="(item2, i2) in cateLevel2" :key="i2">
+    <view class="cate-lv2-title">/ {{item2.cat_name}} /</view>
+    <!-- 动态渲染三级分类的列表数据 -->
+    <view class="cate-lv3-list">
+      <!-- 三级分类 Item 项 -->
+      <view class="cate-lv3-item" v-for="(item3, i3) in item2.children" :key="i3">
+        <!-- 图片 -->
+        <image :src="item3.cat_icon"></image>
+        <!-- 文本 -->
+        <text>{{item3.cat_name}}</text>
+      </view>
+    </view>
+  </view>
+</scroll-view>
     </view>
   </view>
 </template>
@@ -55,6 +36,8 @@
         wh: 0,  //设备视口大小
         cateList: [], //分类数据列表
          active: 0, //选项索引
+         cateLevel2: [], //二级分类列表
+          scrollTop: 0, //滚动条到顶部距离
 
       };
     },
@@ -71,10 +54,15 @@
         const { data: res} = await uni.$http.get('/api/public/v1/categories')
         if (res.meta.status !== 200) return uni.$showMsg()
         this.cateList = res.message
+         // 为二级分类赋值
+          this.cateLevel2 = res.message[0].children
       },
       // 点击分类事件切换
       activeChanged(i){
          this.active = i
+         this.cateLevel2 = this.cateList[i].children
+         // 让 scrollTop 的值在 0 与 1 之间切换
+           this.scrollTop = this.scrollTop === 0 ? 1 : 0
       }
     }
   }
@@ -113,5 +101,34 @@
         }
       }
     }
+    .cate-lv2-title {
+      font-size: 12px;
+      font-weight: bold;
+      text-align: center;
+      padding: 15px 0;
+    }
+    .cate-lv3-list {
+      display: flex;
+      flex-wrap: wrap;
+    
+      .cate-lv3-item {
+        width: 33.33%;
+        margin-bottom: 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    
+        image {
+          width: 60px;
+          height: 60px;
+        }
+    
+        text {
+          font-size: 12px;
+        }
+      }
+    }
+
+
   }
 </style>
