f7f8bd971f3954ecc0635eb5d0051388af4012e1
zjm <2215539769@qq.com>
2023-09-07 15:11:39 +0800

    首页分类导航渲染

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -12,7 +12,7 @@
       </swiper>
       <!-- 分类导航区域 -->
       <view class="nav-list">
-         <view class="nav-item" v-for="(item, i) in navList" :key="i">
+         <view class="nav-item" v-for="(item, i) in navList" :key="i" @click="navClickHandler(item)">
            <image :src="item.image_src" class="nav-img"></image>
          </view>
       </view>
@@ -35,10 +35,8 @@
     methods: {
       // 获取轮播图数据方法
       async getSwiperList() {
-         const { data: res } = await uni.$http.get('/api/public/v1/home/swiperdata')
-  // 3.2 请求失败
-      if (res.meta.status !== 200)   return uni.$showMsg()
-      // 3.3 请求成功，为 data 中的数据赋值
+      const { data: res } = await uni.$http.get('/api/public/v1/home/swiperdata')
+      if (res.meta.status !== 200) {return uni.$showMsg()} 
       this.swiperList = res.message
       },
       /** 获取分类导航数据*/
@@ -46,7 +44,14 @@
          const { data: res } = await uni.$http.get('/api/public/v1/home/catitems')
               if (res.meta.status !== 200) return uni.$showMsg()
               this.navList = res.message
-      }
+      },
+       //分类点击事件
+       navClickHandler(item){
+          // 判断点击的是哪个 nav
+           if (item.name === '分类') {
+             uni.switchTab({ url: '/pages/cate/cate' })
+           }
+       }
     }
   }
 </script>
