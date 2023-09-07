f3b41321e5689d45895a6f8ac8c900aa9bce631e
zjm <2215539769@qq.com>
2023-09-07 20:28:36 +0800

    三级列表跳转完成,分类完成

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -16,10 +16,8 @@
     <!-- 动态渲染三级分类的列表数据 -->
     <view class="cate-lv3-list">
       <!-- 三级分类 Item 项 -->
-      <view class="cate-lv3-item" v-for="(item3, i3) in item2.children" :key="i3">
-        <!-- 图片 -->
+      <view class="cate-lv3-item" v-for="(item3, i3) in item2.children" :key="i3" @click="gotoGoodsList(item3)">
         <image :src="item3.cat_icon"></image>
-        <!-- 文本 -->
         <text>{{item3.cat_name}}</text>
       </view>
     </view>
@@ -63,6 +61,12 @@
          this.cateLevel2 = this.cateList[i].children
          // 让 scrollTop 的值在 0 与 1 之间切换
            this.scrollTop = this.scrollTop === 0 ? 1 : 0
+      },
+      // 三级分类点击事件
+      gotoGoodsList(item3){
+         uni.navigateTo({
+            url: '/subpkg/goods_list/goods_list?cid=' + item3.cat_id
+          })
       }
     }
   }
