f710b44fe3860e2d098294b08decfba0944bc652
zjm <2215539769@qq.com>
2023-09-07 17:31:19 +0800

    完成分类列表渲染

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,6 +1,50 @@
 <template>
   <view>
-    cate
+    <view class="scroll-view-container">
+      <!-- 左侧的滚动视图区域 -->
+      <!-- 左侧的滚动视图区域 -->
+      <scroll-view class="left-scroll-view" scroll-y :style="{height: wh + 'px'}">
+        <block v-for="(item, i) in cateList" :key="i">
+         <view :class="['left-scroll-view-item', i === active ? 'active' : '']"  @click="activeChanged(i)">{{item.cat_name}}</view>
+        </block>
+      </scroll-view>
+
+      <!-- 右侧的滚动视图区域 -->
+      <scroll-view class="right-scroll-view" scroll-y :style="{height: wh + 'px'}">
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">多复制一些节点，演示纵向滚动效果</view>
+      </scroll-view>
+    </view>
   </view>
 </template>
 
@@ -8,12 +52,66 @@
   export default {
     data() {
       return {
-        
+        wh: 0,  //设备视口大小
+        cateList: [], //分类数据列表
+         active: 0, //选项索引
+
       };
+    },
+    onLoad() {
+      // 获取当前系统的信息
+      const sysInfo = uni.getSystemInfoSync()
+      // 为 wh 窗口可用高度动态赋值
+      this.wh = sysInfo.windowHeight
+      this.getCateList()
+    },
+    methods: {
+      //获取分类列表
+      async getCateList() {
+        const { data: res} = await uni.$http.get('/api/public/v1/categories')
+        if (res.meta.status !== 200) return uni.$showMsg()
+        this.cateList = res.message
+      },
+      // 点击分类事件切换
+      activeChanged(i){
+         this.active = i
+      }
     }
   }
 </script>
 
 <style lang="scss">
+  .scroll-view-container {
+    display: flex;
 
+    .left-scroll-view {
+      width: 120px;
+
+      .left-scroll-view-item {
+        line-height: 60px;
+        background-color: #f7f7f7;
+        text-align: center;
+        font-size: 12px;
+
+        // 激活项的样式
+        &.active {
+          background-color: #ffffff;
+          position: relative;
+
+          // 渲染激活项左侧的红色指示边线
+          &::before {
+            content: ' ';
+            display: block;
+            width: 3px;
+            height: 30px;
+            background-color: #c00000;
+            position: absolute;
+            left: 0;
+            top: 50%;
+            transform: translateY(-50%);
+          }
+        }
+      }
+    }
+  }
 </style>
