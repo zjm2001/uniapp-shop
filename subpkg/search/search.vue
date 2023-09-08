252c091ab2e3cf04b197bb2dcf15ad41e354a20c
zjm <2215539769@qq.com>
2023-09-08 15:01:06 +0800

    搜索页面基础布局完成

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -1,7 +1,8 @@
 <template>
-  <view>
-    
-  </view>
+<view class="search-box">
+  <!-- 使用 uni-ui 提供的搜索组件 -->
+  <uni-search-bar @input="input" :radius="100" cancelButton="none"></uni-search-bar>
+</view>
 </template>
 
 <script>
@@ -10,10 +11,21 @@
       return {
         
       };
+    },
+    methods:{
+        input(e) {
+          // e.value 是最新的搜索内容
+          console.log(e.value)
+        }
     }
   }
 </script>
 
 <style lang="scss">
+.search-box {
+  position: sticky;
+  top: 0;
+  z-index: 999;
+}
 
 </style>
