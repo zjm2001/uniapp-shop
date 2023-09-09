7fd9c25804a7e55536ebef383a822d2732f75249
zjm <2215539769@qq.com>
2023-09-09 10:27:30 +0800

    完成了商品列表上拉刷新

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -5,6 +5,9 @@
           <!-- 为 my-goods 组件动态绑定 goods 属性的值 -->
           <my-goods :goods="goods"></my-goods>
       </block>
+      <view class="txt" v-if="isShow">
+        没有更多了~~~~
+      </view>
     </view>
   </view>
 </template>
@@ -22,6 +25,7 @@
              goodsList: [], //商品列表
              total: 0,
              isloading: false, //防止多次频繁发起多次请求
+             isShow:false
       };
     },
     onLoad(options) {
@@ -31,10 +35,11 @@
        this.getGoodsList()
     },
     methods:{
-       async getGoodsList (){
+       async getGoodsList (cb){
            this.isloading=true  //渲染数据打开节流阀
            const { data: res } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
            this.isloading=false  //数据加载完毕关闭节流阀
+           cb && cb()
              if (res.meta.status !== 200) return uni.$showMsg()
              // 为数据赋值
              this.goodsList =[...this.goodsList,... res.message.goods]
@@ -43,10 +48,23 @@
     },
     //触底事件
     onReachBottom() {
-        if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+       if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) {
+           this.isShow=true
+         return  uni.$showMsg('数据加载完毕！')
+       }
+      
       if(this.isloading) return   //判断是否有其他数据在请求
       this.queryObj.pagenum += 1
       this.getGoodsList()
+    },
+    // 下拉刷新的事件
+    onPullDownRefresh() {
+       // 1. 重置关键数据
+        this.queryObj.pagenum = 1
+        this.total = 0
+        this.isloading = false
+        this.goodsList = []
+        this.getGoodsList(() => uni.stopPullDownRefresh())      //  重新发起请求且停止下啦刷新数据
     }
   }
 </script>
@@ -54,5 +72,8 @@
 <style lang="scss">
 .goods-list{
   padding-bottom: 10px;
+  .txt{
+    text-align: center;
+  }
 }
 </style>
