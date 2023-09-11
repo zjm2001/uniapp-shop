cf59e404f96760f0b72ec3c0994832046e0e95cd
zjm <2215539769@qq.com>
2023-09-11 16:50:25 +0800

    完成收货地址区域

--- store/store.js
+++ store/store.js
@@ -2,6 +2,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import m_cart from '@/store/cart.js'
+import m_user from '@/store/user.js'
 
 // 2. 将 Vuex 安装为 Vue 的插件
 Vue.use(Vuex)
@@ -9,7 +10,8 @@ Vue.use(Vuex)
 // 3. 创建 Store 的实例对象
 const store = new Vuex.Store({
   modules: {
-    m_cart
+    m_cart,
+    m_user
   },
   getters:{
     cart:state => state.m_cart.cart,
@@ -18,7 +20,9 @@ const store = new Vuex.Store({
           let c = 0
           state.m_cart.cart.forEach(goods => c += goods.goods_count)
           return c
-       }
+       },
+     address:state=> state.m_user.address,
+     addstr:state=> state.m_user.addstr,
   }
 })
 
---
+++ store/user.js
@@ -0,0 +1,28 @@
+
+const state={
+  address:{}
+}
+const mutations={
+  //更新收货地址
+     updateAddress(state, address) {
+        state.address = address
+         this.commit('m_user/saveAddressToStorage')
+      },
+       // 1. 定义将 address 持久化存储到本地 mutations 方法
+    saveAddressToStorage(state) {
+        uni.setStorageSync('address', JSON.stringify(state.address))
+      },
+}
+const getters={
+     // 收货详细地址的计算属性
+      addstr(state) {
+        if (!state.address.provinceName) return '' 
+        return state.address.provinceName + state.address.cityName + state.address.countyName + state.address.detailInfo    // 拼接 省，市，区，详细地址 的字符串并返回给用户
+      }
+}
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  getters
+}
