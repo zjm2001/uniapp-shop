3271b71cf3439291a666ad55d9840e7790b2b976
zjm <2215539769@qq.com>
2023-09-13 15:32:10 +0800

    完成支付功能(但是接口问题无法支付)

--- components/my-settle/my-settle.vue
+++ components/my-settle/my-settle.vue
@@ -26,7 +26,7 @@
     name: "my-settle",
     computed: {
       ...mapGetters('m_cart', ['checkedCount', 'checkedGoodsAmount']), //勾选的商品的总数量和统计勾选商品总价个
-      ...mapGetters(['total']), //store中的getters导出total和addstr详细地址
+      ...mapGetters(['total','cart']), //store中的getters导出total和addstr详细地址
       ...mapGetters('m_user', ['addstr']),
       ...mapState('m_user', ['token']), // token 是用户登录成功之后的 token 字符串
       //是否全选
@@ -54,6 +54,7 @@
         if (!this.addstr) return uni.$showMsg('请选择收货地址！')
         // if (!this.token) return uni.$showMsg('请先登录！')
         if (!this.token) return this.delayNavigate()
+          this.payOrder()      // 4. 实现微信支付功能
       },
       // 展示倒计时的提示消息
       showTips(n) {
@@ -65,7 +66,7 @@
           duration: 1500 // 1.5 秒后自动消失
         })
       },
-
+      //实现点击展示消息然后跳转登录
       delayNavigate() {
         this.seconds = 5
         this.showTips(this.seconds) // 1. 展示提示消息，此时 seconds 的值等于 3
@@ -91,8 +92,32 @@
           }
           this.showTips(this.seconds) // 再根据最新的秒数，进行消息提示
         }, 1000)
-      }
-
+      },
+      //支付功能
+   async payOrder() {
+     const orderInfo = {
+       // 开发期间，注释掉真实的订单价格， // 写死订单总价为 1 分钱   // order_price: this.checkedGoodsAmount,
+       order_price: 0.01,
+       consignee_addr: this.addstr,
+       goods: this.cart.filter(x => x.goods_state).map(x => ({ goods_id: x.goods_id, goods_number: x.goods_count, goods_price: x.goods_price }))
+     }
+     const { data: res } = await uni.$http.post('/api/public/v1/my/orders/create', orderInfo)    // 1.2 发起请求创建订单
+     if (res.meta.status !== 200) return uni.$showMsg('创建订单失败！')
+     const orderNumber = res.message.order_number       // 1.3 得到服务器响应的“订单编号”
+      // 2. 订单预支付
+    const { data: res2 } = await uni.$http.post('/api/public/v1/my/orders/req_unifiedorder', { order_number: orderNumber })
+     if (res2.meta.status !== 200) return uni.$showError('预付订单生成失败！')
+     const payInfo = res2.message.pay
+      // 3. 发起微信支付
+      const [err, succ] = await uni.requestPayment(payInfo)
+       if (err) return uni.$showMsg('订单未支付！')
+        const { data: res3 } = await uni.$http.post('/api/public/v1/my/orders/chkOrder', { order_number: orderNumber })
+          if (res3.meta.status !== 200) return uni.$showMsg('订单未支付！')
+          uni.showToast({
+               title: '支付完成！',
+               icon: 'success'
+             })
+    }
     }
   }
 </script>
